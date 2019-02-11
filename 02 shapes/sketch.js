ed782df35be625eb956efa4e157caa55ce292a15
Dong4202392 <4202392@sps-student.sk.ca>
2019-02-11 10:08:34 -0600

    Update sketch.js

--- 02 shapes/sketch.js
+++ 02 shapes/sketch.js
@@ -8,13 +8,27 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  x = width/2
+  y = height/2
+  xSpeed = random(-3,3);
+  ySpeed = random(-3,3);
+  background(255)
+  noStroke();
 }
 
 function draw() {
   background(220);
   ellipse(MOUSEx,MOUSEY,25,25)
+  x += xSpeed
+  y += ySpeed
+  dill(random(255), random(255), random(255))
+
 }
-
-
+function mousePressed(){
+  fill (255,0,0);
+  xSpeed = random(-3,3;)
+  ySpeed = random(-3,3;)
+}
+function mouseReleased(){\fill(0,0,255);}
 }
 
