9532dabc85d2e93dacc67b3c149270531df20a41
Dong4202392 <4202392@sps-student.sk.ca>
2019-02-12 10:40:29 -0600

    03

--- 03/sketch.js
+++ 03/sketch.js
@@ -5,11 +5,31 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let img;
+let imgSize;
+function preload(){
+  img = loadImage("assets/gear.png");
 
+}
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
+  imgSize = 200;
 }
 
 function draw() {
   background(220);
+  image(img,mouseX,mouseY,imgSize,imgSize);
+}
+function mouseWheel(event){
+  if(event.delta > 0){
+  imgSize += 25;
+
+} 
+  else{
+    if(imgSize > 25){
+    imgSize -= 25;
+  
+  }
+}
 }
