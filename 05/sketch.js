79447d3acf462163fe487e4b1d212b6ba04d0d32
Dong4202392 <4202392@sps-student.sk.ca>
2019-02-13 10:52:34 -0600

    Update sketch.js

--- 05/sketch.js
+++ 05/sketch.js
@@ -5,11 +5,36 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let state = 0; // 0-top 1-right 2- bottom 3-left
+let rectSize = 30;
+let x = 0;
+let y = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
-
 function draw() {
   background(220);
+  if (state === 0){//top
+    x += 10
+      if (x > windowWidth - rectSize) state = 1;
+
+
 }
+else if (state === 1){// right 
+  y += 10
+   if (y > windowHeight - rectSize ) state = 2
+
+
+}
+else if (state === 2){// bottom 
+  x -= 10
+   if (x <= 0) state = 3
+}
+else if (state === 3){// left 
+  y -= 10
+   if (y <= 0) state = 4
+}
+rect(x,y,rectSize,rectSize)
+}
+
