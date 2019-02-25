7701a93ee1177460890c998082a37a19f4713c5e
Dong4202392 <4202392@sps-student.sk.ca>
2019-02-25 10:04:29 -0600

    07

--- 06/sketch.js
+++ 06/sketch.js
@@ -4,12 +4,37 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+let x = 200;
+let y = 200;
+let rSize = 50;
+let pickedUp = false;
+let mouseOver = false;
+
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER)
 }
 
 function draw() {
-  background(220);
+  background(0);
+  fill (150,0,0);
+  mouseOver = false;
+  if (mouseX >= x - rSize/2 && mouseX <= x + rSize/2 );
+   if (mouseY >= y - rSize/2 && mouseY <= y + rSize/2 );
+   fill(255,0,0);
+   mouseOver = true;
+
+  if (pickedUp === true);
+  x=mouseX;
+  y=mouseY;
+  rect(x,y,rSize,rSize);
+}
+function mousePressed(){
+  if (mouseOver === true) pickedUp = true;
+}
+
+function mouseReleased(){
+  pickedUp = false;
 }
--- Primitive Paint/sketch.js
+++ Primitive Paint/sketch.js
@@ -4,16 +4,24 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
-
+let randomS;  // random size 
+let randomX; // random X position 
+let randomY; // random Y position
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textSize(32);
   textStyle(BOLD);
   text('word', 10, 30);
-  fill(0, 102, 153);  
+
+
  
 }
+function draw (){
+  randomX = random(0,1500)
+  randomS = random(0,100)
+  randomY = random(0,800)
+}
+
 function keyTyped(){
   if (key==="a")
   rect(mouseX,mouseY,75,100);
@@ -22,8 +30,9 @@ function keyTyped(){
   ellipse(mouseX,mouseY,75,100);
   if (key==="d")
   arc(mouseX,mouseY,80, 80, 0, PI + QUARTER_PI);
-  fill(random(255));
-  if (key === "")
-  rect();
-  
+  fill(random(0,255),random(0,255),random(0,255),);
+  if (key === " ")
+  background(255)
+  if (key==="p")
+  rect(randomX,randomY,randomS,randomS);
 }
