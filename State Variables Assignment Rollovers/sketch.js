9484b3106e8de8b5597c6919a99ee45a0b0a9414
Dong4202392 <4202392@sps-student.sk.ca>
2019-02-26 11:04:13 -0600

    Update sketch.js

--- State Variables Assignment Rollovers/sketch.js
+++ State Variables Assignment Rollovers/sketch.js
@@ -11,5 +11,20 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(255);
+  line(width/2,0,width/2,height)
+  line(0,height/2,width,height/2)
+  if(mouseX <= width/2 && mouseY <= height/2 )
+   rect(0,0,width/2,height/2)
+   fill(0)
+  if(mouseX > width/2 && mouseY >= height/2 )
+   rect(width/2,height/2,width,height)
+   fill(0)
+  if(mouseX <= width/2 && mouseY > height/2 )
+   rect(0,height/2,width/2,height)
+   fill(0) 
+  if(mouseX > width/2 && mouseY < height/2 )
+   rect(width/2,0,width,height/2)
+   fill(0) 
+   
 }
