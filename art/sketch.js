73a71ac74f977b0e184c3c6bb04377cdaa984614
yilindong22 <47329898+yilindong22@users.noreply.github.com>
2019-05-28 14:30:56 -0600

    pp

--- art/sketch.js
+++ art/sketch.js
@@ -1,6 +1,5 @@
-let w
-let h
-
+let w // width of the ellipse in the middle
+let h // height of the ellipe in the middle
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
@@ -12,11 +11,10 @@ function draw() {
   flower();
 }
 
-function flower() {
+function flower() { // the function to draw the flower
   w = tan(frameCount) * 100;
   h = sin(frameCount) * 100;
-  translate(width/2, height/2);
-  
+  translate(width/2, height/2); // posstion 
   for (i = 0; i < 3; i++) {
     rotate(radians(360 / 3));
     ellipse(0, 0, w, h);
