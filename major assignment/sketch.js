697bb16d95c5a87e86767dcfd71989487e40e933
yilindong22 <47329898+yilindong22@users.noreply.github.com>
2019-05-29 11:02:47 -0600

    ..

--- major assignment/sketch.js
+++ major assignment/sketch.js
@@ -2,30 +2,34 @@
 let direction = 1 
 let x = 100;
 let y = 600; 
-let currentImage = 0; 
+let currentImage = 0;
 walk = [] 
+shoot = []
+let shooot = 0
+
 function preload() {
-  for (let i = 0; i <= 8; i++){ 
+  for (let i = 0; i <= 13; i++){ 
     walk.push(loadImage("assets/walk/0" + i + ".png"));             
   }
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(10) // speed switch images
+  frameRate(8) // speed switch images
 }
 
 function draw() {
   background(220);
   walkk()
+  print(shooot)
 }
 
 function walkk(){ 
   imageMode(CENTER); 
   push();
-  translate(x, y); // move trump
-  scale(1.5); // size 
-  if (direction === 1){  // if direction = 1 then trump move left 
+  translate(x, y); 
+  scale(1.5);  
+  if (direction === 1){  
     scale(-1,1)
     if(currentImage >= 8 ){
       currentImage = 1;
@@ -42,11 +46,35 @@ function walkk(){
       }
 
   }
-  print(currentImage,direction)
+  if (shooot >= 1){
+    if (direction === 3){
+      currentImage === 9
+    if(currentImage >= 13){
+      shooot = 0 
+    }
+    else{
+      currentImage ++
+    }
+   }
+    if(direction === 4){
+      scale(-1,1)
+    currentImage === 9
+    if(currentImage >= 13){
+      shooot = 0 
+    }
+    else{
+      currentImage ++
+    }
+   }
+   }
+    
+    
   image(walk[currentImage], 0, 0);
   pop();
+
 }
 
+  
 function keyPressed(){
   if (keyCode === RIGHT_ARROW){ // right side
     x += 20;
@@ -56,5 +84,13 @@ function keyPressed(){
     x -= 20;
     direction = 1
   }
+  if(keyCode === UP_ARROW ){
+    if(direction = 1){
+      direction =3 
+    }
+    if (direction === 2){
+      direction = 4
+    } 
+    shooot += 1
+  }
 }
-
